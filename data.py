3cea3ee336198de6698a5500f9c2e9c4dc5b0456
joanafo <132621765+joanafo@users.noreply.github.com>
2024-04-21 17:19:57 -0700

    Segunda entrega

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-fbabf440-2563-4318-813a-e75ece6398fc.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-d02f9d17-046e-440e-a61b-c2aea7c509a6.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import data
+import helpers ##Importar el archivo con los métodos de apoyo
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -8,41 +9,29 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 
-
-
-# no modificar
-def retrieve_phone_code(driver) -> str:
-    """Este código devuelve un número de confirmación de teléfono y lo devuelve como un string.
-    Utilízalo cuando la aplicación espere el código de confirmación para pasarlo a tus pruebas.
-    El código de confirmación del teléfono solo se puede obtener después de haberlo solicitado en la aplicación."""
-
-    import json
-    import time
-    from selenium.common import WebDriverException
-    code = None
-    for i in range(10):
-        try:
-            logs = [log["message"] for log in driver.get_log('performance') if log.get("message")
-                    and 'api/v1/number?number' in log.get("message")]
-            for log in reversed(logs):
-                message_data = json.loads(log)["message"]
-                body = driver.execute_cdp_cmd('Network.getResponseBody',
-                                              {'requestId': message_data["params"]["requestId"]})
-                code = ''.join([x for x in body['body'] if x.isdigit()])
-        except WebDriverException:
-            time.sleep(1)
-            continue
-        if not code:
-            raise Exception("No se encontró el código de confirmación del teléfono.\n"
-                            "Utiliza 'retrieve_phone_code' solo después de haber solicitado el código en tu aplicación.")
-        return code
-
-
+#Todos los selectores se movieron al inicio de las pruebas y se cambio el tipo de selector
 class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
-    taxi_button_locator = (By.CSS_SELECTOR, "#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button")
+    taxi_button_locator = (By.XPATH, "//*[@class='button round' and text()='Pedir un taxi']")  #Selector único para el botón
+    comfort_tariff_locator = (By.XPATH, "//*[@class='tcard-title' and text()='Comfort']")
+    glam_tariff_locator = (By.XPATH, "//*[@class='tcard-title' and text()='Glamuroso']")
+    phone_button_locator = (By.XPATH, "//*[@class='np-text' and text()='Número de teléfono']")
+    phone_number_field_locator = (By.XPATH, "//*[@id='phone']")
+    next_button_locator = (By.XPATH, "//*[@class='button full' and text()='Siguiente']") #Selector único para el botón
+    code_locator = (By.XPATH, "//*[@id='code']")
+    button_confirm_locator = (By.XPATH, "//*[@class='button full' and text()='Confirmar']") #Selector único para el botón
+    payment_method_button_locator = (By.XPATH, "//*[@class='pp-text' and text()='Método de pago']")
+    credit_card_locator = (By.XPATH, "//*[@class='pp-title' and text()='Agregar tarjeta']")
+    card_number_field_locator = (By.XPATH, "//*[@id='number']")
+    cvv_number_field_locator = (By.XPATH, "//*[@id='code']")
+    message_field_locator = (By.XPATH, "//*[@id='comment']")
+    slider_locator = (By.XPATH, "//*[@class='slider round']") #Selector único para el slider
+    increment_button_locator = (By.XPATH, "//*[@class='counter-plus']") #Selector único para el contador
+    button_order_locator = (By.XPATH, "//*[@class='smart-button']") #Selector único para el botón
 
     def __init__(self, driver):
         self.driver = driver
@@ -64,36 +53,29 @@ class UrbanRoutesPage:
         taxi_button.click()
 
     def click_comfort_tariff(self):
-        comfort_tariff_locator = (By.XPATH, "//*[@class='tcard-title' and text()='Comfort']")
-        comfort_tariff = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(comfort_tariff_locator))
+        comfort_tariff = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.comfort_tariff_locator))
         comfort_tariff.click()
 
     def click_glam_tariff(self):
-        comfort_tariff_locator = (By.XPATH, "//*[@class='tcard-title' and text()='Glamuroso']")
-        comfort_tariff = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(comfort_tariff_locator))
+        comfort_tariff = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.glam_tariff_locator))
         comfort_tariff.click()
 
     def click_phone(self):
-        phone_button_locator = (By.XPATH, "//*[@class='np-text' and text()='Número de teléfono']")
-        phone_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(phone_button_locator))
+        phone_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.phone_button_locator))
         phone_button.click()
 
     def fill_phone_number(self, phone_number):
         wait = WebDriverWait(self.driver, 10)
-        phone_number_field_locator = (By.XPATH, "//*[@id='phone']")
-        phone_number_field = wait.until(ec.visibility_of_element_located(phone_number_field_locator))
+        phone_number_field = wait.until(ec.visibility_of_element_located(self.phone_number_field_locator))
         phone_number_field.clear()
         phone_number_field.send_keys(phone_number)
-        next_button_locator = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]/form/div[2]/button')
-        next_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(next_button_locator))
+        next_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.next_button_locator))
         next_button.click()
-        code = retrieve_phone_code(self.driver)
-        code_locator = (By.XPATH, "//*[@id='code']")
-        code_field = wait.until(ec.visibility_of_element_located(code_locator))
+        code = helpers.retrieve_phone_code(self.driver)
+        code_field = wait.until(ec.visibility_of_element_located(self.code_locator))
         code_field.clear()
         code_field.send_keys(code)
-        button_confirm_locator = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[2]/form/div[2]/button[1]')
-        button_confirm = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(button_confirm_locator))
+        button_confirm = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.button_confirm_locator))
         button_confirm.click()
 
     def get_phone_number(self):
@@ -102,53 +84,42 @@ class UrbanRoutesPage:
         return phone_number_field.get_attribute('value')
 
     def click_payment_method_card_button(self):
-        payment_method_button_locator = (By.XPATH, "//*[@class='pp-text' and text()='Método de pago']")
-        payment_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(payment_method_button_locator))
+        payment_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.payment_method_button_locator))
         payment_button.click()
-        credit_card_locator = (By.XPATH, "//*[@class='pp-title' and text()='Agregar tarjeta']")
-        card_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(credit_card_locator))
+        card_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.credit_card_locator))
         card_button.click()
 
     def fill_credit_number_card(self, card):
         wait = WebDriverWait(self.driver, 10)
-        card_number_field_locator = (By.XPATH, "//*[@id='number']")
-        card_number_field = wait.until(ec.visibility_of_element_located(card_number_field_locator))
+        card_number_field = wait.until(ec.visibility_of_element_located(self.card_number_field_locator))
         card_number_field.clear()
         card_number_field.send_keys(card)
         card_number_field.send_keys(Keys.TAB)
-        time.sleep(1)
 
     def fill_credit_cvv_card(self, cvv):
         wait = WebDriverWait(self.driver, 10)
-        cvv_number_field_locator = (By.XPATH, "//*[@id='code']")
-        cvv_number_field = wait.until(ec.visibility_of_element_located(cvv_number_field_locator))
+        cvv_number_field = wait.until(ec.visibility_of_element_located(self.cvv_number_field_locator))
         cvv_number_field.clear()
         cvv_number_field.send_keys(cvv)
         cvv_number_field.send_keys(Keys.TAB)
-        time.sleep(1)
 
     def send_message(self, message):
         wait = WebDriverWait(self.driver, 10)
-        message_field_locator = (By.XPATH, "//*[@id='comment']")
-        message_field = wait.until(ec.visibility_of_element_located(message_field_locator))
+        message_field = wait.until(ec.visibility_of_element_located(self.message_field_locator))
         message_field.clear()
         message_field.send_keys(message)
 
     def click_blanket_scarves(self):
-        slider_locator = (By.XPATH, "//*[@id='root']/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div/span")
-        slider = WebDriverWait(self.driver, 10).until(ec.visibility_of_element_located(slider_locator))
+        slider = WebDriverWait(self.driver, 10).until(ec.visibility_of_element_located(self.slider_locator))
         slider.click()
 
     def click_order_icecream(self):
-        increment_button_locator = (By.XPATH,"//*[@id='root']/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]")
-        increment_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(increment_button_locator))
+        increment_button = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.increment_button_locator))
         for _ in range(data.ice_cream):
             increment_button.click()
-            time.sleep(1)
 
     def click_order_taxi(self):
-        button_order_locator = (By.XPATH, "//*[@id='root']/div/div[3]/div[4]/button/span[1]")
-        button_order = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(button_order_locator))
+        button_order = WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(self.button_order_locator))
         button_order.click()
 
 
@@ -162,10 +133,10 @@ class TestUrbanRoutes:
         options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
         cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-
+#Se elimino el uso de "time.sleep()" y se agrego el método "assert" en todas las pruebas
     def test_set_route(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(3)
+        time.sleep(1)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -176,7 +147,6 @@ class TestUrbanRoutes:
 
     def test_set_comfort_tariff(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(2)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -187,11 +157,9 @@ class TestUrbanRoutes:
         comfort_tariff_element = WebDriverWait(self.driver, 10).until(
             ec.visibility_of_element_located((By.CSS_SELECTOR, "#root > div > div.workflow > div.workflow-subcontainer > div.tariff-picker.shown > div.tariff-cards > div.tcard.active > div.tcard-title")))
         assert "Comfort" in comfort_tariff_element.text, "La tarifa de comfort no se ha seleccionado"
-        time.sleep(2)
     
     def test_phone_number(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(2)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -204,11 +172,10 @@ class TestUrbanRoutes:
         routes_page.fill_phone_number(phone_number_test)
         actual_phone_number = routes_page.get_phone_number()
         assert actual_phone_number == phone_number_test, f"Número actual: {actual_phone_number}"
-        time.sleep(2)
 
+#Se agrego assert
     def test_credit_card(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(2)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -224,11 +191,14 @@ class TestUrbanRoutes:
         cvv_test = data.card_code
         routes_page.fill_credit_number_card(card_numer_test)
         routes_page.fill_credit_cvv_card(cvv_test)
-        time.sleep(2)
+
+        card_number_field = WebDriverWait(self.driver, 10).until(ec.visibility_of_element_located(routes_page.card_number_field_locator))
+        assert card_number_field.get_attribute('value') == card_numer_test, "El número de tarjeta no coincide."
+        cvv_number_field = WebDriverWait(self.driver, 10).until(ec.visibility_of_element_located(routes_page.cvv_number_field_locator))
+        assert cvv_number_field.get_attribute('value') == cvv_test, "El CVV no coincide."
 
     def test_message_driver(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(2)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -238,7 +208,6 @@ class TestUrbanRoutes:
         routes_page.click_comfort_tariff()
         message_test = data.message_for_driver
         routes_page.send_message(message_test)
-        time.sleep(2)
         comment_field_locator = (By.XPATH, "//*[@id='comment']")
         comment_field = WebDriverWait(self.driver, 10).until(ec.visibility_of_element_located(comment_field_locator))
         actual_message = comment_field.get_attribute('value')
@@ -246,7 +215,6 @@ class TestUrbanRoutes:
 
     def test_order_blanket_scarves(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(2)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -255,11 +223,10 @@ class TestUrbanRoutes:
         routes_page.click_taxi_button()
         routes_page.click_comfort_tariff()
         routes_page.click_blanket_scarves()
-        time.sleep(2)
 
+#Se agrego assert
     def test_order_ice_cream(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(2)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -268,11 +235,13 @@ class TestUrbanRoutes:
         routes_page.click_taxi_button()
         routes_page.click_comfort_tariff()
         routes_page.click_order_icecream()
-        time.sleep(2)
-    
+        counter_element = WebDriverWait(self.driver, 10).until(ec.visibility_of_element_located((By.CLASS_NAME, "counter-value")))
+        counter = counter_element.text
+        assert counter == data.ice_cream, f"La cantidad no coincide"
+
+#Se agrego assert
     def test_order_taxi(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(2)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -283,14 +252,21 @@ class TestUrbanRoutes:
         routes_page.click_phone()
         phone_number_test = data.phone_number
         routes_page.fill_phone_number(phone_number_test)
-        actual_phone_number = routes_page.get_phone_number()
-        assert actual_phone_number == phone_number_test, f"Número actual: {actual_phone_number}"
         routes_page.click_order_taxi()
-        time.sleep(5)
-   
+        try:
+            # Verifica si el botón sigue siendo clickeable después de hacer clic
+            is_clicked = WebDriverWait(self.driver, 10).until(
+                ec.element_to_be_clickable(UrbanRoutesPage.button_order_locator)).is_enabled()
+        except StaleElementReferenceException:
+            # Si el elemento ya no está presente en el DOM, se produce una excepción
+            is_clicked = False
+
+            # Utiliza un assert para verificar si el botón fue clickeado correctamente
+        assert is_clicked, "No se pudo confirmar que se hizo clic en el botón Order Taxi."
+
+
     def test_order_taxi_info(self):
         self.driver.get(data.urban_routes_url)
-        time.sleep(2)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
@@ -301,8 +277,6 @@ class TestUrbanRoutes:
         routes_page.click_phone()
         phone_number_test = data.phone_number
         routes_page.fill_phone_number(phone_number_test)
-        actual_phone_number = routes_page.get_phone_number()
-        assert actual_phone_number == phone_number_test, f"Número actual: {actual_phone_number}"
         routes_page.click_order_taxi()
         time.sleep(60)
 
